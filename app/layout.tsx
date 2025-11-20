0aad4bf20354132da62fdb9f42a24414df08e2e4
saba-bar95 <saba.barb95@gmail.com>
2025-11-20 22:37:22 +0400

    lesson 7

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,6 +8,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <head>
+        <title>Next.js Dashboard</title>
+      </head>
       <body className={`${inter.className} antialiased`}>{children}</body>
     </html>
   );
